4540b703a9903c9c7f3faf254eed8423ae148f2c
mohammed sebti <mohammed.sebti@agqlabs.com>
2024-11-10 19:05:18 +0100

    first commit2dffffh3

--- dentist/settings.py
+++ dentist/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url 
+#import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +89,15 @@ DATABASES = {
 }
 """
 DATABASES = {
-    'default': dj_database_url.config(
-        default=DATABASE_URL,  # Railway configura automáticamente DATABASE_URL
-        conn_max_age=1800
-    ),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dEABbCgyqqXRNQvxJZwlKeFtzcCjYFSg',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
